6b02472a5b5c3710fb6a8890fc1675724c8420e7
thispc <peezacub@hotmail.com>
2024-07-12 16:20:58 +0700

    add mysql2

---
+++ src/app/db/index.ts
@@ -0,0 +1,6 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DATABASE_NAME ?? '', process.env.DATABASE_USERNAME ?? '', process.env.DATABASE_PASSWORD, {
+	host: 'localhost',
+	dialect: 'mysql',
+});
